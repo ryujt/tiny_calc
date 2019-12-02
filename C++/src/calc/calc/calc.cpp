6024cec749feea1651c0637adc8c82acf9438efc
Ryu <ryujt658@hanmail.net>
2019-12-02 23:27:03 +0900

    괄호 처리 추가

--- C++/src/calc/calc/calc.cpp
+++ C++/src/calc/calc/calc.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
+#include <string>
 #include "calculator.hpp"
+
+using namespace std;
 
 int main()
 {
 	Calculator calculator;
 
-	//string text = "1 + 3 * 2";
-	string text = "1 + 3 * 2 - 1 + 5";
-	//string text = "2 * (1.5 + 3)";
-	printf("%s = %g", text.c_str(), calculator.execute(text));
+	string text;
+	//text = "1 + 3 * 2";
+	//text = "1 + 3 * 2 - 1 + 5";
+	//text = "2 * (1.5 + 0.5)";
+	//text = "(1 + 2) * (1.5 + 0.5)";
+
+	while (true) {
+		getline(cin, text);
+		if (text == "q") break;
+		printf("%s = %g \n", text.c_str(), calculator.execute(text));
+	}
 }
